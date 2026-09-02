32b85fcbe3640f8b10e129614a462792ca75930d
bensuoztat <bensu.oztat@gmail.com>
2026-09-02 12:28:44 +0300

    best_pred error fixed

--- app/model.py
+++ app/model.py
@@ -1,4 +1,5 @@
 #model yükle hafızada tut
+"""
 from transformers import pipeline
 
 #hf hızlı inglizce sent analysis modeli
@@ -11,3 +12,28 @@ def predictSentiment(text: str): #dışardan metin alıp sent analyisis yapar
         "label": result["label"],
         "score": round(float(result["score"]),4) #skoru 4 basmaağa yuvarlar 
         }
+
+"""
+
+import requests
+API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+
+def get_score(prediction):
+    return prediction['score']
+
+def predictSentiment(text: str):
+    try:
+        response = requests.post(API_URL, json={"inputs": text})
+        result =response.json()
+
+        if isinstance(result, list) and len(result) > 0:
+            predictions = result[0]
+
+            best_pred = max(predictions, key=get_score)
+            
+            return {"label": best_pred['label'], "score": round(best_pred['score'], 4)}
+        
+        return {"status": "Model yükleniyor veya API yanıt vermedi", "detail": result}
+
+    except Exception as e:
+        return {"error": str(e)}
